873496b0ea72485fdd3e41c4d8a32136fd39c161
McVienna <markus.vogl96otmail.com>
2017-11-27 03:11:59 +0100

    started working in prototypeScene - nothing done yet

---
+++ Bit-Z/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+    
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
