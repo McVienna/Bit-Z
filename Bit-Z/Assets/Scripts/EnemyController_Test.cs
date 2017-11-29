6ff7779a1bfcf01da62b1d149cb1f6baa656331c
McVienna <markus.vogl96@hotmail.com>
2017-11-29 13:31:06 +0100

    added test_enemy animation -> need to create simple AI

---
+++ Bit-Z/Assets/Scripts/EnemyController_Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController_Test : MonoBehaviour { 
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    /// <summary>
+    /// No Use of delta-time because FixedUpdate function -> called for working with rigidbody!
+    /// FixedUpdate is called once per Frame!
+    /// </summary>
+    void FixedUpdate()
+    {
+
+    }
+}
