a6923077567aedb5ae98d9c36a3a16bcc977a081
McVienna <markus.vogl96otmail.com>
2017-12-08 19:52:38 +0100

    Camer no follows Player (need polish) -> Scrolling Background needs to be reworked doesnt work yetgit add .git add .

--- Bit-Z/Assets/Scripts/GameManager.cs
+++ Bit-Z/Assets/Scripts/GameManager.cs
@@ -6,12 +6,17 @@ public class GameManager : MonoBehaviour {
 
     public GameObject m_PLayerPrefab;
     public GameObject m_EnemyPrefab;
+    public GameObject m_MainCamera;
+    public GameObject m_Background;
 
 	// Use this for initialization
 	void Start ()
     {
         Instantiate(m_PLayerPrefab);
         Instantiate(m_EnemyPrefab);
+        Instantiate(m_Background);
+        Instantiate(m_MainCamera);
+
 	}
 	
 	// Update is called once per frame
