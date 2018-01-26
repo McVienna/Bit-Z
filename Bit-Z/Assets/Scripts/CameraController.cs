f2849056fab71d67e14c5e91999dbca80aab154e
Stefan <stefan.muehlbacher91@gmail.com>
2018-01-26 18:33:50 +0100

    Mit dem Camera SCript angefangen

--- Bit-Z/Assets/Scripts/CameraController.cs
+++ Bit-Z/Assets/Scripts/CameraController.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject Player;
+    public Vector2 velocity;
+    public float smoothTimex;
+    public float smoothTimey;
+    public bool bounds;
 
-    private Vector3 offset;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+    private float minY;
+    private float maxY, minX, maxX;
+
+    //private Vector3 offset;
 
     // Use this for initialization
-	void Start ()
+    void Start ()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
-
-        offset = transform.position - Player.transform.position;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = Player.transform.position + offset;
-	}
+
+
+        var camw = GetComponent<Camera>().orthographicSize;
+        //var camh = GetComponent<Camera>().pixelHeight;
+        //minCameraPos = new Vector3(minY, 0, -3);
+
+        Debug.Log("Ortho: " + camw);
+
+        float posX = Mathf.SmoothDamp(transform.position.x, Player.transform.position.x, ref velocity.x, smoothTimex);
+        float posY = Mathf.SmoothDamp(transform.position.y, Player.transform.position.y, ref velocity.y, smoothTimey);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                                             Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                                             Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+            Debug.Log("Transform: " + transform.position);
+        }
+
+    }
 }
